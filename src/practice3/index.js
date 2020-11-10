1a0e8b07d130d9041dfcd2e492f0a1d47744eb27
YumingChen <mathcym@outlook.com>
2020-11-10 22:13:34 +0800

    [yuming chen]: finish practice 1, 2, 3.

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const { origin, author, content } = data;
+      return [origin, author, content];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
